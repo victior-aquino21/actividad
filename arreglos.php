86a8c243f027ba59f101f3b5b6b0e6180593bc89
victior-aquino21 <victoraquino5121@gmail.com>
2026-03-26 12:43:04 -0600

    arreglos de compu

---
+++ arreglos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>arreglos PHP</title>
+</head>
+<body>
+    <div class="container">
+        <h1>arreglos en PHP</h1>
+
+<?php
+    $Nums = [101,103,105,107];
+    echo $Nums[0];
+    ?>
+
+
+<?php
+   $trabajador =[
+"nombre"=>"luis",
+"edad"=> 32, 
+"salario" => 5000
+echo $trabajador["nombre"];
+    ]:
+    ?>
+
+    <php
+    $frutas=[manzana,banana,naranja];
+
+    foreach($frutas as $pocoyo){
+        echo $frutas;
+        $pocoyo
+}
